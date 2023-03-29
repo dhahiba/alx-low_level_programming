0c96a08c57572db22ecedc7b0eea8536d93a32fd
dhahiba <dhhiba1991@gmail.com>
2023-03-29 15:54:29 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters
+ * @str: String
+ * Return: String
+ */
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+		index++;
+	}
+	return (str);
+}
