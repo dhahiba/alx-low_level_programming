95b8e05633af3aa58c3f876a9dd2afa2a9846b71
dhahiba <dhhiba1991@gmail.com>
2023-03-29 14:15:34 -0700

    task0

---
+++ 0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcat - Concatenates two strings
+ * @dest: string
+ * @src: string
+ * return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dlen = 0, i;
+
+	while (dest[dlen])
+		dlen++;
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[dlen] = src[i];
+		dlen++;
+	}
+	dest[dlen] = '\0';
+	return (dest);
+}
