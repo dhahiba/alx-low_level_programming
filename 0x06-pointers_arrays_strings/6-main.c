a84c3b104144a06c727c22863262d41603c91037
dhahiba <dhhiba1991@gmail.com>
2023-03-29 17:32:12 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -8,8 +8,8 @@
  */
 int main(void)
 {
-char str[] = "Expect the best. Prepare for the worst. ", "+",
-	"Capitalize on what comes.\nhello world! hello-world " +
+char str[] = "Expect the best. Prepare for the worst.\t"
+	"Capitalize on what comes.\nhello world! hello-world\t"
 	"0123456hello world\thello world.hello world\n";
 char *ptr;
 
