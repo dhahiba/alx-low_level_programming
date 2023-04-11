9738d9e7d4c12ffd2cf574e8c329a5882e98076b
dhahiba <dhhiba1991@gmail.com>
2023-04-11 12:12:24 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers
+ * @argc: input
+ * @argv: input
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int x = 0, y = 0;
+
+	if (argc == 3)
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		printf("%d\n", x * y);
+	}
+	else
+	{
+		printf("error\n");
+		return (1);
+	}
+	return (0);
+}
