351f53930420d71e40ec32c6c7374828dad42029
dhahiba <dhhiba1991@gmail.com>
2023-04-12 03:36:05 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,13 +9,12 @@
  */
 int main(int argc, char *argv[])
 {
-	int x = 0, y = 0;
+	int x = 0;
 
 	if (argc == 3)
 	{
-		x = atoi(argv[1]);
-		y = atoi(argv[2]);
-		printf("%d\n", x * y);
+		x = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", x);
 	}
 	else
 	{
