d6e6a11b9dc06869f213c8b2ad36f68fcaa378fe
dhahiba <dhhiba1991@gmail.com>
2023-04-13 15:03:52 -0700

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -30,10 +30,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	void *m;
 
 	if (size == 0 || nmemb == 0)
-		return (0);
+		return (NULL);
 	m = malloc(sizeof(int) * nmemb);
 	if (m == 0)
-		return (0);
+		return (NULL);
 	_memset(m, 0, sizeof(int) * nmemb);
 	return (m);
 }
