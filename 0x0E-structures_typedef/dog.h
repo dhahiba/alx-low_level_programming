b9cd185b2e786433903bf8b01188d4fcbcc08e94
dhahiba <dhhiba1991@gmail.com>
2023-04-17 13:02:43 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - ...
+ * @name: ...
+ * @age: ...
+ * @owner: ...
+ * Description: ...
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
