5224066c71051731cb18d5fa89eb5be50b568d03
dhahiba <dhhiba1991@gmail.com>
2023-04-29 13:52:22 -0700

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -45,8 +45,34 @@ printf("%s%s", separator, str);
  * @format: ...
  * Return: ...
  */
-void print_all(const char * const format, ...);
+void print_all(const char * const format, ...)
 {
 	int i = 0, j;
-	
+	char *separator = "";
+	va_list ap;
+	token_t tokens[] = {
+		{"c", format_char},
+		{"i", format_int},
+		{"f", format_float},
+		{"s", format_string},
+		{NULL, NULL}
+	};
+
+	va_start(ap, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (tokens[j].token)
+		{
+		if (format[i] == tokens[j].token[0])
+		{
+		tokens[j].f(separator, ap);
+		separator = ", ";
+		}
+		j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(ap);
 }
