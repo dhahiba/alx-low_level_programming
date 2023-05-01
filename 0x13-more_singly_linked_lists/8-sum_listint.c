b56e7594bb67ce8df85e63bb530948fe49a331a6
dhahiba <dhhiba1991@gmail.com>
2023-05-01 10:16:43 -0700

    task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * sum_listint - ....
+ * @head: ...
+ * Return: ...
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
