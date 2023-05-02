ddbf6684084c9a3349407a61663bfa85c718b128
dhahiba <dhhiba1991@gmail.com>
2023-05-02 03:41:51 -0700

    task 12

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -26,7 +26,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 size_t print_listint(const listint_t *h);
 listint_t *reverse_listint(listint_t **head);
-listint_t *reverse_listint(listint_t **head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+size_t print_listint_safe(const listint_t *head);
+const listint_t **tr(const listint_t **list, size_t size, const listint_t *new);
 #endif
