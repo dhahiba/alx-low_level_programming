85f0de03450530797a64ed4b93dbff9932970d49
dhahiba <dhhiba1991@gmail.com>
2023-05-04 10:36:35 -0700

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * binary_to_uint - ...
+ * @b: ...
+ * Return: ...
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n = 0;
+	int i;
+
+	if (!b)
+		return (0);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		n = 2 * n + (b[i] - '0');
+	}
+	return (n);
+}
