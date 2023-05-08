59bfd13aa3a40b8c3ef31d7cc65cf179fea3acc7
dhahiba <dhhiba1991@gmail.com>
2023-05-08 16:07:29 -0700

    task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ if (text_content != NULL)
 for (len = 0; text_content[len];)
 len++;
 }
-f = open(filename, O_CREAT | O_RDWR | O_TRUNC);
+f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 w = write(f, text_content, len);
 if (f == -1 || w == -1)
 return (-1);
